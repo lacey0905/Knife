9e398ca47faf760bc2cbf3656a4e5bb9e54a773d
NEOWIZ\hgkim0905 <lacey0905@naver.com>
2018-11-12 17:29:11 +0900

    GameManager

---
+++ Assets/Scripts/CGameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CGameManager : MonoBehaviour {
+
+
+    private void Awake()
+    {
+        
+    }
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            
+
+
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //RaycastHit hit;
+            //if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            //{
+            //    if (hit.transform.tag.Equals("Temp"))
+            //    {
+            //        Debug.Log("충돌");
+            //    }
+            //}
+        }
+	}
+}
