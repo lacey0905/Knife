7abf976c9ff75ebe7de80e22650e7091357373b4
NEOWIZ\hgkim0905 <lacey0905@naver.com>
2018-11-14 18:19:14 +0900

    Knife 프로토타입

--- Assets/Scripts/CGameManager.cs
+++ Assets/Scripts/CGameManager.cs
@@ -13,7 +13,7 @@ public class CGameManager : MonoBehaviour {
     Vector3 m_startMousePoint = Vector3.zero;
     Vector3 m_currentMousePoint = Vector3.zero;
     float m_fDistance = 0.0f;
-    float m_fMaxDistance = 2.0f;
+    float m_fMaxDistance = 1.0f;
 
     bool isTouch = false;
 
@@ -24,22 +24,45 @@ public class CGameManager : MonoBehaviour {
 
     public List<GameObject> m_KnifeList = new List<GameObject>();
 
+    private LineRenderer lineRenderer;
+
+    public List<CObject> m_Object = new List<CObject>();
+
     private void Awake()
     {
+        lineRenderer = GetComponent<LineRenderer>();
     }
 
     private void Start()
     {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             GameObject Temp = Instantiate(m_Knife, pos.transform.position, Quaternion.identity) as GameObject;
             Temp.transform.parent = pos.transform;
             m_KnifeList.Add(Temp);
         }
         m_KnifeList[0].SetActive(true);
+
+        StartCoroutine(StartObject());
     }
 
-
     public int count = 0;
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    int objCount = 0;
+
+    IEnumerator StartObject()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            m_Object[objCount].gameObject.SetActive(true);
+            objCount++;
+            yield return new WaitForSeconds(2.0f);
+        }
+    }
 
     void Update ()
     {
@@ -47,8 +70,29 @@ public class CGameManager : MonoBehaviour {
         {
             if (Input.GetMouseButtonDown(0))
             {
-                m_startMousePoint = this.getMousePoint();
-                isTouch = true;
+                if (count >= m_KnifeList.Count-1)
+                {
+                    for (int i = 0; i < m_KnifeList.Count-1; i++)
+                    {
+                        m_KnifeList[i].GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                        m_KnifeList[i].transform.position = pos.transform.position;
+                        m_KnifeList[i].SetActive(false);
+                    }
+                    m_KnifeList[0].SetActive(false);
+                    count = 0;
+                }
+                else
+                {
+                    int layerMask = 1 << LayerMask.NameToLayer("Knife"); 
+                    Vector2 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    Ray2D ray = new Ray2D(wp, Vector2.zero);
+                    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity, layerMask);
+                    if (hit.collider != null && hit.collider.tag == "Start")
+                    {
+                        m_startMousePoint = this.getMousePoint();
+                        isTouch = true;
+                    }
+                }
             }
         }
         else
@@ -62,11 +106,15 @@ public class CGameManager : MonoBehaviour {
                     m_fDistance = Vector3.Distance(m_startMousePoint, m_currentMousePoint);
                     if (m_fDistance > m_fMaxDistance)
                     {
-                        m_Direction = getDirection(m_currentMousePoint, m_startMousePoint);
+                        m_Direction = getDirection(m_currentMousePoint, pos.transform.position);
                         Rigidbody2D t = m_KnifeList[count].GetComponent<Rigidbody2D>();
                         t.velocity = m_Direction * m_fPower;
 
                         t.transform.up = m_Direction;
+
+
+                        lineRenderer.SetPosition(0, pos.transform.position);
+                        lineRenderer.SetPosition(1, m_currentMousePoint);
 
                         count++;
                         //StartCoroutine("setKnife");
@@ -86,17 +134,9 @@ public class CGameManager : MonoBehaviour {
         }
     }
 
-    IEnumerator setKnife()
-    {
-        yield return new WaitForSeconds(0.2f);
-        m_KnifeList[count].SetActive(true);
-    }
-
     Vector2 getDirection(Vector3 _head, Vector3 _tail)
     {
         Vector2 dir = (_head - _tail).normalized;
-        dir.x = Mathf.Clamp(dir.x, -0.5f, 0.5f);
-        dir.y = Mathf.Clamp(dir.y, -0.5f, 0.5f);
         return dir;
     }
 
---
+++ Assets/Scripts/CObject.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CObject : MonoBehaviour {
+
+    Rigidbody2D rid;
+
+    float x;
+    float y;
+    float r;
+
+    Vector2 startPos;
+
+    void Awake()
+    {
+        rid = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        startPos = this.transform.position;
+        setObject();
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+    public void setObject()
+    {
+        x = Random.Range(-1f, 1f);
+        y = Random.Range(10f, 15f);
+        r = Random.Range(-50f, 50f);
+        rid.velocity = new Vector2(x, y);
+        rid.AddTorque(r);
+    }
+
+    public void Reset()
+    {
+        rid.velocity = Vector2.zero;
+        this.transform.position = startPos;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Knife")
+        {
+            this.gameObject.SetActive(false);
+            Reset();
+            setObject();
+        }
+    }
+}
